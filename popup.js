f02ac23277e35b1f4e4348bb06624558356f6103
alex <>
2026-04-10 14:47:19 +0100

    better layout

--- popup.js
+++ popup.js
@@ -417,20 +417,22 @@ function renderSnapshots(snapshots) {
           </a>
           <div class="snapshot-preview">${escapeHtml(snapshot.textPreview)}</div>
           <div class="snapshot-actions">
-            <button class="btn-small btn-copy" data-id="${snapshot.id}" aria-label="Copy">
-              <svg class="btn-icon-item" viewBox="-0.5 -0.5 16 16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="16" height="16">
-                <path d="M6.875 5.625h5.625s1.25 0 1.25 1.25v5.625s0 1.25 -1.25 1.25h-5.625s-1.25 0 -1.25 -1.25v-5.625s0 -1.25 1.25 -1.25" stroke-width="1"></path>
-                <path d="M3.125 9.375H2.5a1.25 1.25 0 0 1 -1.25 -1.25V2.5a1.25 1.25 0 0 1 1.25 -1.25h5.625a1.25 1.25 0 0 1 1.25 1.25v0.625" stroke-width="1"></path>
-              </svg>
-            </button>
+
             <button class="btn-small btn-delete" data-id="${snapshot.id}" aria-label="Delete">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2" id="Trash-2--Streamline-Feather" height="16" width="16">
+              <svg class="btn-icon-item" viewBox="-0.5 -0.5 16 16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2" id="Trash-2--Streamline-Feather" height="16" width="16">
                 <path d="m1.875 3.75 1.25 0 10 0" stroke-width="1"></path>
                 <path d="M11.875 3.75v8.75a1.25 1.25 0 0 1 -1.25 1.25H4.375a1.25 1.25 0 0 1 -1.25 -1.25V3.75m1.875 0V2.5a1.25 1.25 0 0 1 1.25 -1.25h2.5a1.25 1.25 0 0 1 1.25 1.25v1.25" stroke-width="1"></path>
                 <path d="m6.25 6.875 0 3.75" stroke-width="1"></path>
                 <path d="m8.75 6.875 0 3.75" stroke-width="1"></path>
               </svg>
             </button>
+            <button class="btn-small btn-copy" data-id="${snapshot.id}" aria-label="Copy">
+              <svg class="btn-icon-item" viewBox="-0.5 -0.5 16 16" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="16" height="16">
+                <path d="M6.875 5.625h5.625s1.25 0 1.25 1.25v5.625s0 1.25 -1.25 1.25h-5.625s-1.25 0 -1.25 -1.25v-5.625s0 -1.25 1.25 -1.25" stroke-width="1"></path>
+                <path d="M3.125 9.375H2.5a1.25 1.25 0 0 1 -1.25 -1.25V2.5a1.25 1.25 0 0 1 1.25 -1.25h5.625a1.25 1.25 0 0 1 1.25 1.25v0.625" stroke-width="1"></path>
+              </svg>
+            </button>
+
           </div>
         </div>
       `;
